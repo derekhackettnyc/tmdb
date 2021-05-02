7f9b0b6518218984837b237ed8b6dfe38527c62a
derek hackett <derekhackettnyc@gmail.com>
2021-05-02 11:44:33 -0400

    added class color--yellow

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,8 +8,8 @@ const Home = () => {
 
             <section className="home">
                 <h1 className="main-heading">
-                    <span className="main-heading--main moveInLeft">React Movies/TV</span>
-                    <span className="main-heading--sub moveInRight">Movies, TV shows and people to discover</span>
+                    <span className="main-heading--main moveInLeft color--yellow">React Movies/TV</span>
+                    <span className="main-heading--sub moveInRight color--yellow">Movies, TV shows and people to discover</span>
                 </h1>
             </section>
 
