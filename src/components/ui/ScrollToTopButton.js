0d430a61783ebad1b0e67928d139a2689f010a76
derek hackett <derekhackettnyc@gmail.com>
2021-05-03 07:40:08 -0400

    button to scroll back to top of page

---
+++ src/components/ui/ScrollToTopButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScrollToTopButton = () => {
+    return(
+        <button className='trueblue'>Scroll To Top</button>
+    )
+}
+
+export default ScrollToTopButton
