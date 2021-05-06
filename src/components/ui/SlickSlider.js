e961f1649e055fc765cfbb3251343e45d93a99b2
derek hackett <derekhackettnyc@gmail.com>
2021-05-05 20:23:31 -0400

    added Link to slides

--- src/components/ui/SlickSlider.js
+++ src/components/ui/SlickSlider.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Link } from 'react-router-dom'
 import Slider from "react-slick"
 
 import { IMAGE_BASE_URL, PROFILE_SIZE, BACKDROP_SIZE } from '../../config'
@@ -11,7 +12,7 @@ export default class SlickSlider extends Component {
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 3,
-      initialSlide:0,
+      initialSlide: 0,
       // arrows: true,
       responsive: [
         {
@@ -34,16 +35,18 @@ export default class SlickSlider extends Component {
     };
     return (
       <Slider {...settings}>
-        {this.props.slides.map(({id, backdrop_path, title, vote_average }) => (
-          <div className="course-details" key={id}>
-            <div className="course-details__picture">
-              <img src={`${IMAGE_BASE_URL}${PROFILE_SIZE}${backdrop_path}`} alt={title} />
+        {this.props.slides.map(({ id, backdrop_path, title, vote_average }) => (
+          <Link to={`/${'movie'}/${id}`}>
+            <div className="course-details" key={id}>
+              <div className="course-details__picture">
+                <img src={`${IMAGE_BASE_URL}${PROFILE_SIZE}${backdrop_path}`} alt={title} />
+              </div>
+              <div className="course-details__main">
+                <h2 className="course-details__title">{title}</h2>
+                <p>{vote_average} <i className="fas fa-star" style={{ color: 'orange' }}></i></p>
+              </div>
             </div>
-            <div className="course-details__main">
-              <h2 className="course-details__title">{title}</h2>
-              <p>{vote_average} <i className="fas fa-star" style={{ color: 'orange' }}></i></p>
-            </div>
-          </div>
+          </Link>
         ))}
       </Slider>
     )
